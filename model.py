c228fa9a2565816cf63750118465b7676e2bd7d9
Anthony Margetic <amargeti@u.rochester.edu>
2016-12-01 20:18:09 -0500

    added file access in model

--- model.py
+++ model.py
@@ -1,11 +1,18 @@
 #Premade 
 import datetime
+import os
 
 #Custom
 import submit
 
 
 def main():
-	 submit.sumbit_results()
+	classes = os.listdir("train")[1:]
+	print classes
+	for i in range(len(classes)):
+		print len(os.listdir('train/' + classes[i]))
+	
+	#submit.sumbit_results()
+
 
 main()
