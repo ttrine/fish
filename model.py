fee9dc7461305094a317bf17b01dc5f8ab17c120
Tyler Trine <trine.tyler@gmail.com>
2016-12-04 19:16:57 -0500

    Add train/test split to preprocess

--- model.py
+++ model.py
@@ -1,18 +1,58 @@
-#Premade 
-import datetime
-import os
+import numpy as np
+import random
+import pandas as pd
 
-#Custom
-import submit
+from sklearn.metrics import log_loss
+
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Convolution2D, MaxPooling2D, ZeroPadding2D, Dense, Activation
+from keras.optimizers import RMSprop, Adam
+from keras.callbacks import EarlyStopping
+from keras import backend as K
+
+from preprocess import TRAIN_DIR,TEST_DIR,FISH_CLASSES,ROWS,COLS,CHANNELS
+
+X_all = np.load('data/train/np_imgs.npy')
+y_all = np.load('data/train/np_labels.npy')
+
+optimizer = RMSprop(lr=1e-4)
+objective = 'categorical_crossentropy'
+
+def center_normalize(x):
+    return (x - K.mean(x)) / K.std(x)
+
+model = Sequential()
+
+model.add(Activation(activation=center_normalize, input_shape=(ROWS, COLS, CHANNELS)))
+
+model.add(Convolution2D(32, 5, 5, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(Convolution2D(32, 5, 5, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
+
+model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
+
+model.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu', dim_ordering='tf'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
 
 
-def main():
-	classes = os.listdir("train")[1:]
-	print classes
-	for i in range(len(classes)):
-		print len(os.listdir('train/' + classes[i]))
-	
-	#submit.sumbit_results()
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
 
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
 
-main()
+model.add(Dense(len(FISH_CLASSES)))
+model.add(Activation('sigmoid'))
+
+model.compile(loss=objective, optimizer=optimizer)
