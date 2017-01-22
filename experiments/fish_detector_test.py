a5849cc3a27c00453ae4da965b2463b284d7514a
Tyler Trine <trine.tyler@gmail.com>
2017-01-22 01:25:54 -0500

    Bugfix in fish_detector_test

--- experiments/fish_detector_test.py
+++ experiments/fish_detector_test.py
@@ -7,26 +7,26 @@ from fish.detector_container import ModelContainer
 
 def construct(n):
 	model = Sequential()
-	
+
 	model.add(BatchNormalization(axis=3, input_shape=(n, n, 3)))
 	model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
 	model.add(Convolution2D(4, 3, 3, activation='relu', dim_ordering='tf'))
 	model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
 	model.add(Convolution2D(4, 3, 3, activation='relu', dim_ordering='tf'))
 	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='tf'))
-	
+
 	model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
 	model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='tf'))
 	model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
 	model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='tf'))
 	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='tf'))
-	
+
 	model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
 	model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='tf'))
 	model.add(ZeroPadding2D((1, 1), dim_ordering='tf'))
 	model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='tf'))
 	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='tf'))
-	
+
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu'))
 	model.add(Dropout(0.5))
@@ -35,7 +35,7 @@ def construct(n):
 	model.add(Dense(32, activation='relu'))
 	model.add(Dropout(0.5))
 	model.add(Dense(1, activation='softmax'))
-	
+
 	return model
 
 sgd = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 	import sys
 
 	if len(sys.argv) > 1: # very very basic
-		model = ModelContainer('fish_detector_test',construct(sys.argv[1]),sgd)
+		model = ModelContainer('fish_detector_test',construct(int(sys.argv[1])),sgd)
 		model.isfish_train(n=int(sys.argv[1]), nb_epoch=int(sys.argv[2]), samples_per_epoch=int(sys.argv[3]), nb_val_samples=int(sys.argv[4]))
 	else:
 		model = ModelContainer('fish_detector_test',construct(256),sgd)
