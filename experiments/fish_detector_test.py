01da6c25f54f176219341cbcb73ff95d3eb7ed60
Tyler Trine <trine.tyler@gmail.com>
2017-01-23 18:23:21 -0500

    Change some default

--- experiments/fish_detector_test.py
+++ experiments/fish_detector_test.py
@@ -43,8 +43,8 @@ if __name__ == '__main__':
 	import sys
 
 	if len(sys.argv) > 1: # very very basic
-		model = ModelContainer('fish_detector_test',construct(int(sys.argv[1])),sgd)
-		model.isfish_train(n=int(sys.argv[1]), nb_epoch=int(sys.argv[2]), batch_size=int(sys.argv[3]), samples_per_epoch=int(sys.argv[4]))
+		model = ModelContainer('fish_detector_test',construct(int(sys.argv[1])),sys.argv[1],sgd)
+		model.isfish_train(nb_epoch=int(sys.argv[2]), batch_size=int(sys.argv[3]), samples_per_epoch=int(sys.argv[4]))
 	else:
-		model = ModelContainer('fish_detector_test',construct(256),sgd)
-		model.isfish_train(n=256, nb_epoch=100, batch_size=500, samples_per_epoch=1000)
+		model = ModelContainer('fish_detector_test',construct(256),256,sgd)
+		model.isfish_train(nb_epoch=100, batch_size=500, samples_per_epoch=1000)
