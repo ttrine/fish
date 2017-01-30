808c7a0caa0228813769bcc0a2f11d0cb1b58369
Tyler Trine <trine.tyler@gmail.com>
2017-01-30 18:32:05 -0500

    detect_batch_adam_128 and detect_batch_adam_64 in more or less final form

--- experiments/detect_batch_adam_128/train.py
+++ experiments/detect_batch_adam_128/train.py
@@ -43,5 +43,5 @@ if __name__ == '__main__':
 	import sys # basic arg parsing, infer ModelContainer name
 	name = sys.argv[0].split('/')[-2]
 
-	model = ModelContainer(name,construct(64),64,"adam")
+	model = ModelContainer(name,construct(128),128,"adam")
 	model.isfish_train(nb_epoch=int(sys.argv[1]), batch_size=int(sys.argv[2]), samples_per_epoch=int(sys.argv[3]))
