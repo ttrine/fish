ca94d40a6b7bd6079c999b499804acf5c7bb5a44
Tyler Trine <trine.tyler@gmail.com>
2017-01-31 12:54:43 -0500

    Basic error handling for CL args in experiments

--- experiments/template/train.py
+++ experiments/template/train.py
@@ -4,6 +4,10 @@ def construct(n):
 if __name__ == '__main__':
 	import sys # basic arg parsing, infer ModelContainer name
 	name = sys.argv[0].split('/')[-2]
+	
+	if len(sys.argv) < 4:
+		print "Usage: train nb_epoch batch_size samples_per_epoch"
+		sys.exit()
 
-	model = ModelContainer(name,construct(64),64,"adam")
-	model.isfish_train(nb_epoch=int(sys.argv[1]), batch_size=int(sys.argv[2]), samples_per_epoch=int(sys.argv[3]))
+	model = ModelContainer(name,construct(64),64,"sgd")
+	model.train(nb_epoch=int(sys.argv[1]), batch_size=int(sys.argv[2]), samples_per_epoch=int(sys.argv[3]))
