6fc29bccb50b872564b67e579a192f230e48fa4e
Tyler Trine <trine.tyler@gmail.com>
2017-01-31 12:54:09 -0500

    Change chunk to a global in detector_container for greater flexibility

--- fish/detector_container.py
+++ fish/detector_container.py
@@ -29,6 +29,50 @@ def overlap(c_x1,c_x2,c_y1,c_y2,a_x1,a_x2,a_y1,a_y2):
 	y_overlap = len(set(range(c_y1,c_y2)).intersection(set(range(a_y1,a_y2))))>0
 	return x_overlap and y_overlap
 
+def chunk(n,y_boxes,img,mask,filename):
+	# Insert augmentation here
+
+	ncol = int(math.ceil(float(1732)/float(n)))
+	nrow = int(math.ceil(float(974)/float(n)))
+
+	# Chunk up image. If chunk has coverage, it will be present once for each box it covers.
+	img_chunks = []
+	chunk_labels = []
+	filenames = [] # For easier debugging
+	for j in range(ncol):
+		for i in range(nrow):
+			x1 = j*n
+			y1 = i*n
+			x2 = ((j+1)*n)
+			y2 = ((i+1)*n)
+			img_chunk = img[y1:y2,x1:x2]
+			if not np.any(img_chunk): continue # skip all-black chunks
+			if img_chunk.shape != (n,n,3): # skip the rare case in which bottom/right-most chunks are nonblack
+				continue
+			mask_chunk = mask[y1:y2,x1:x2]
+			pct_coverage = float(sum(sum((mask_chunk>0).astype(np.uint8))))/float(n*n)
+			if pct_coverage < .001: # not enough of fish in chunk, label as no fish
+				img_chunks.append(img_chunk)
+				chunk_labels.append(np.array([0,0,0,0,0]))
+				filenames.append(filename)
+			else: # compute relative top-left and bottom-right bounding-box coords for each fish
+				if filename in y_boxes: # just in case it isn't
+					annotations = y_boxes[filename]
+					for annotation in annotations:
+						if overlap(x1,x2,y1,y2,annotation['x1'],annotation['x2'],annotation['y1'],annotation['y2']):
+							# only add annotation if fish is in chunk
+							x_center = int(round(np.mean([x1,x2])))
+							y_center = int(round(np.mean([y1,y2])))
+							x_dist_tr = annotation['x1'] - x_center
+							y_dist_tr = annotation['y1'] - y_center
+							x_dist_br = annotation['x2'] - x_center
+							y_dist_br = annotation['y2'] - y_center
+							img_chunks.append(img_chunk)
+							chunk_labels.append(np.array([x_dist_tr,y_dist_tr,x_dist_br,y_dist_br,1]))
+							filenames.append(filename)
+				else: continue
+	return (img_chunks,chunk_labels,filenames)
+
 class ModelContainer:
 	def __init__(self,name,model,n,optimizer=Adam(lr=1e-5)):
 		self.name = name
@@ -57,50 +101,6 @@ class ModelContainer:
 		self.X_eval = eval_data['X']
 		self.filenames_eval = np.load("data/test_stg1/binary/y_filenames.npy")
 
-	def chunk(self,img,mask,filename):
-		# Insert augmentation here
-
-		ncol = int(math.ceil(float(1732)/float(self.n)))
-		nrow = int(math.ceil(float(974)/float(self.n)))
-
-		# Chunk up image. If chunk has coverage, it will be present once for each box it covers.
-		img_chunks = []
-		chunk_labels = []
-		filenames = [] # For easier debugging
-		for j in range(ncol):
-			for i in range(nrow):
-				x1 = j*self.n
-				y1 = i*self.n
-				x2 = ((j+1)*self.n)
-				y2 = ((i+1)*self.n)
-				img_chunk = img[y1:y2,x1:x2]
-				if not np.any(img_chunk): continue # skip all-black chunks
-				if img_chunk.shape != (self.n,self.n,3): # skip the rare case in which bottom/right-most chunks are nonblack
-					continue
-				mask_chunk = mask[y1:y2,x1:x2]
-				pct_coverage = float(sum(sum((mask_chunk>0).astype(np.uint8))))/float(self.n*self.n)
-				if pct_coverage < .001: # not enough of fish in chunk, label as no fish
-					img_chunks.append(img_chunk)
-					chunk_labels.append(np.array([0,0,0,0,0]))
-					filenames.append(filename)
-				else: # compute relative top-left and bottom-right bounding-box coords for each fish
-					if filename in self.y_boxes: # just in case it isn't
-						annotations = self.y_boxes[filename]
-						for annotation in annotations:
-							if overlap(x1,x2,y1,y2,annotation['x1'],annotation['x2'],annotation['y1'],annotation['y2']):
-								# only add annotation if fish is in chunk
-								x_center = int(round(np.mean([x1,x2])))
-								y_center = int(round(np.mean([y1,y2])))
-								x_dist_tr = annotation['x1'] - x_center
-								y_dist_tr = annotation['y1'] - y_center
-								x_dist_br = annotation['x2'] - x_center
-								y_dist_br = annotation['y2'] - y_center
-								img_chunks.append(img_chunk)
-								chunk_labels.append(np.array([x_dist_tr,y_dist_tr,x_dist_br,y_dist_br,1]))
-								filenames.append(filename)
-					else: continue
-		return (img_chunks,chunk_labels,filenames)
-
 	def sample_gen(self,batch_size,X,y_masks,y_filenames): # Yield only coverage indicator
 		def sample_gen(batch_size,X,y_masks,y_filenames): # Yield full BB label
 			random.seed(1) # For reproducibility
@@ -113,7 +113,7 @@ class ModelContainer:
 				img = X[index]
 				mask = y_masks[index]
 				filename = y_filenames[index].split('/')[-1]
-				img_chunks,chunk_labels,filename = self.chunk(img,mask,filename)
+				img_chunks,chunk_labels,filename = chunk(self.n,self.y_boxes,img,mask,filename)
 				chunks.extend(img_chunks)
 				labels.extend(chunk_labels)
 				filenames.extend(filename)
