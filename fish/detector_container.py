6556b8c6ca42e86a379a883483f6a06bf5a09323
Tyler Trine <trine.tyler@gmail.com>
2017-02-05 12:36:45 -0500

    Move chunk routines to separate file and differentiate between train and test chunking

--- fish/detector_container.py
+++ fish/detector_container.py
@@ -1,11 +1,13 @@
 import os, sys
-import csv, random, math
+import csv, random
 import numpy as np
 import h5py
 from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 from keras import backend as K
+
+from fish.chunk import *
 
 def read_boxes():
 	import csv
@@ -22,58 +24,6 @@ def read_boxes():
 			d[box['filename']]=[{'x1':box['x1'],'y1':box['y1'],'x2':box['x2'],'y2':box['y2']}]
 	y_boxes = d
 	return y_boxes
-
-def overlap(c_x1,c_x2,c_y1,c_y2,a_x1,a_x2,a_y1,a_y2):
-	# Returns true only if regions horizontal and vertical regions both overlap
-	x_overlap = len(set(range(c_x1,c_x2)).intersection(set(range(a_x1,a_x2))))>0
-	y_overlap = len(set(range(c_y1,c_y2)).intersection(set(range(a_y1,a_y2))))>0
-	return x_overlap and y_overlap
-
-def chunk(n,y_boxes,img,mask,filename):
-	# Insert augmentation here
-
-	ncol = int(math.ceil(float(1732)/float(n)))
-	nrow = int(math.ceil(float(974)/float(n)))
-
-	# Chunk up image. If chunk has coverage, it will be present once for each box it covers.
-	img_chunks = []
-	chunk_labels = []
-	filenames = [] # For easier debugging
-	locations = [] # For evaluation pipeline
-	for j in range(ncol):
-		for i in range(nrow):
-			x1 = j*n
-			y1 = i*n
-			x2 = ((j+1)*n)
-			y2 = ((i+1)*n)
-			img_chunk = img[y1:y2,x1:x2]
-			if not np.any(img_chunk): continue # skip all-black chunks
-			if img_chunk.shape != (n,n,3): # skip the rare case in which bottom/right-most chunks are nonblack
-				continue
-			mask_chunk = mask[y1:y2,x1:x2]
-			pct_coverage = float(sum(sum((mask_chunk>0).astype(np.uint8))))/float(n*n)
-			if pct_coverage < .001: # not enough of fish in chunk, label as no fish
-				img_chunks.append(img_chunk)
-				chunk_labels.append(np.array([0,0,0,0,0]))
-				filenames.append(filename)
-			else: # compute relative top-left and bottom-right bounding-box coords for each fish
-				if filename in y_boxes: # just in case it isn't
-					annotations = y_boxes[filename]
-					for annotation in annotations:
-						if overlap(x1,x2,y1,y2,annotation['x1'],annotation['x2'],annotation['y1'],annotation['y2']):
-							# only add annotation if fish is in chunk
-							x_center = int(round(np.mean([x1,x2])))
-							y_center = int(round(np.mean([y1,y2])))
-							x_dist_tr = annotation['x1'] - x_center
-							y_dist_tr = annotation['y1'] - y_center
-							x_dist_br = annotation['x2'] - x_center
-							y_dist_br = annotation['y2'] - y_center
-							img_chunks.append(img_chunk)
-							chunk_labels.append(np.array([x_dist_tr,y_dist_tr,x_dist_br,y_dist_br,1]))
-							filenames.append(filename)
-							locations.append((i,j))
-				else: continue
-	return (img_chunks,chunk_labels,filenames,locations)
 
 class ModelContainer:
 	def __init__(self,name,model,n,optimizer=Adam(lr=1e-5)):
@@ -115,7 +65,7 @@ class ModelContainer:
 				img = X[index]
 				mask = y_masks[index]
 				filename = y_filenames[index].split('/')[-1]
-				img_chunks,chunk_labels,filename,_ = chunk(self.n,self.y_boxes,img,mask,filename)
+				img_chunks,chunk_labels,filename = chunk_train(self.n,self.y_boxes,img,mask,filename)
 				chunks.extend(img_chunks)
 				labels.extend(chunk_labels)
 				filenames.extend(filename)
