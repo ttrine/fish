f4a35cef0c76858d0a90966f44a6272338fc981f
Tyler Trine <trine.tyler@gmail.com>
2017-02-12 14:47:12 -0500

    Write sequencer and precompution routine for in preparation for recurrent detector training

--- fish/detect_prechunk.py
+++ fish/detect_prechunk.py
@@ -1,65 +1,53 @@
-import math, h5py
 import numpy as np
+import h5py, pandas
 
-from fish.detect import read_boxes, overlap
+from keras.utils import np_utils
+from keras.preprocessing.sequence import pad_sequences
+
+from fish.chunk import chunk_mask, chunk_image
+from fish.sequence import detector_sequencer
 
 data = h5py.File('data/train/binary/data.h5','r')
 
 X_test = data['X_test']
 y_masks_test = data['y_masks_test']
-y_boxes = read_boxes()
-y_filenames_test = np.load('data/train/binary/y_filenames_test.npy','r')
 
-def chunk(img,mask,filename,n):
-	ncol = int(math.ceil(float(1732)/float(n)))
-	nrow = int(math.ceil(float(974)/float(n)))
-	img_chunks = []
-	chunk_labels = []
-	for j in range(ncol):
-		for i in range(nrow):
-			x1 = j*n
-			y1 = i*n
-			x2 = ((j+1)*n)
-			y2 = ((i+1)*n)
-			img_chunk = img[y1:y2,x1:x2]
-			if not np.any(img_chunk): continue
-			if img_chunk.shape != (n,n,3): continue
-			mask_chunk = mask[y1:y2,x1:x2]
-			pct_coverage = float(sum(sum((mask_chunk>0).astype(np.uint8))))/float(n*n)
-			if pct_coverage < .001: # not enough of fish in chunk, label as no fish
-				img_chunks.append(img_chunk)
-				chunk_labels.append(np.array([0,0,0,0,0]))
-			else: # compute relative top-left and bottom-right bounding-box coords for each fish
-				if filename in y_boxes: # just in case it isn't
-					annotations = y_boxes[filename]
-					for annotation in annotations:
-						if overlap(x1,x2,y1,y2,annotation['x1'],annotation['x2'],annotation['y1'],annotation['y2']):
-							# only add annotation if fish is in chunk
-							x_center = int(round(np.mean([x1,x2])))
-							y_center = int(round(np.mean([y1,y2])))
-							x_dist_tr = annotation['x1'] - x_center
-							y_dist_tr = annotation['y1'] - y_center
-							x_dist_br = annotation['x2'] - x_center
-							y_dist_br = annotation['y2'] - y_center
-							img_chunks.append(img_chunk)
-							chunk_labels.append(np.array([x_dist_tr,y_dist_tr,x_dist_br,y_dist_br,1]))
-				else: continue
-	return (img_chunks,chunk_labels)
+# Convert classes to 1-hot encoding
+y_classes_test = np.load('data/train/binary/y_classes_test.npy')
+y_classes_test = np_utils.to_categorical(pandas.factorize(y_classes_test, sort=True)[0])
 
 def compute_and_write(n):
-	chunks = []
-	labels = []
+	chunk_sequences = []
+	coverage_sequences = []
+	location_sequences = []
+	class_labels = []
+	print "Generating sequences..."
 	for i in range(len(X_test)):
-		filename = y_filenames_test[i].split('/')[-1]
-		img_chunks,chunk_labels = chunk(X_test[i],y_masks_test[i],filename,n)
-		chunks.extend(img_chunks)
-		labels.extend(chunk_labels)
+		chunk_matrix = chunk_image(n,X_test[i])
+		coverage_matrix = chunk_mask(n,chunk_matrix,y_masks_test[i])
 
-	chunks = np.array(chunks)
-	labels = np.array(labels)
+		class_labels.append(y_classes_test[i])
 
-	np.save("data/train/binary/X_test_chunks_"+str(n),chunks)
-	np.save("data/train/binary/y_test_chunks_"+str(n),labels)
+		chunk_sequence, coverage_sequence, location_sequence = detector_sequencer(chunk_matrix, coverage_matrix)
+		
+		chunk_sequences.append(chunk_sequence)
+		coverage_sequences.append(coverage_sequence)
+		location_sequences.append(location_sequence)
+
+	print "Padding sequences (takes a while)..."
+	chunk_sequences = pad_sequences(chunk_sequences)
+	coverage_sequences = pad_sequences(coverage_sequences)
+	location_sequences = pad_sequences(location_sequences)
+	class_labels = np.array(class_labels)
+
+	print chunk_sequences.shape
+	print coverage_sequences.shape
+	print location_sequences.shape
+	print class_labels.shape
+	np.save("data/train/binary/X_test_det_chunk_seqs_"+str(n),chunk_sequences)
+	np.save("data/train/binary/X_test_det_coverage_seqs_"+str(n),coverage_seqs)
+	np.save("data/train/binary/X_test_det_loc_seqs_"+str(n),location_sequences)
+	np.save("data/train/binary/y_test_det_classes_onehot_fish_"+str(n),class_labels)
 
 if __name__ == '__main__':
 	import sys
--- fish/sequence.py
+++ fish/sequence.py
@@ -22,3 +22,25 @@ def random_sequencer(chunks,coverage):
 	location_seq = location_seq[shuffle]
 
 	return chunk_seq, location_seq
+
+# Put all nonblack image chunks into a random sequence
+def detector_sequencer(chunks, coverage):
+	location_seq = []
+	for i in range(len(chunks)):
+		for j in range(len(chunks[i])):
+			if chunks[i,j] is not None:
+				location_seq.append((i, j))
+
+	chunk_seq = [chunks[l] for l in location_seq]
+	coverage_seq = [coverage[l] for l in location_seq]
+	
+	chunk_seq = np.array(chunk_seq)
+	coverage_seq = np.array(coverage_seq)
+	location_seq = np.array(location_seq)
+
+	shuffle = random.sample(range(len(chunk_seq)),len(chunk_seq))
+	chunk_seq = chunk_seq[shuffle]
+	coverage_seq = coverage_seq[shuffle]
+	location_seq = location_seq[shuffle]
+	
+	return np.array(chunk_seq), np.array(coverage_seq), np.array(location_seq)
