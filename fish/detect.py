9f5482ef191c1939998eeeb0adb4eaec70fd2791
Tyler Trine <trine.tyler@gmail.com>
2017-02-15 17:13:16 -0500

    Implement data augmentation. Add detect_aug_128 experiment to test an extreme form of this.

--- fish/detect.py
+++ fish/detect.py
@@ -4,30 +4,18 @@ import numpy as np
 import h5py
 from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint
+
 from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.image import ImageDataGenerator
+
 from keras.optimizers import Adam
 from keras import backend as K
 
 from fish.chunk import chunk_mask, chunk_image
 from fish.sequence import detector_sequencer
 
-def read_boxes():
-	y_boxes_file = open("data/train/binary/y_boxes.csv",'rb')
-	box_reader = csv.reader(y_boxes_file)
-	box_reader.next() # skip header
-	y_boxes = [{'filename': box[0].split('/')[-1],'x1':int(box[1]),'y1':int(box[2]),'x2':int(box[3]),'y2':int(box[4])} for box in box_reader]
-	y_boxes_file.close()
-	d = {}
-	for box in y_boxes:
-		if box['filename'] in d:
-			d[box['filename']].append({'x1':box['x1'],'y1':box['y1'],'x2':box['x2'],'y2':box['y2']})
-		else:
-			d[box['filename']]=[{'x1':box['x1'],'y1':box['y1'],'x2':box['x2'],'y2':box['y2']}]
-	y_boxes = d
-	return y_boxes
-
 class DetectorContainer:
-	def __init__(self,name,model,n,optimizer=Adam(lr=1e-5)):
+	def __init__(self,name,model,n,optimizer=Adam(lr=1e-5),datagen_args=dict()):
 		self.name = name
 
 		model.compile(optimizer=optimizer, loss="binary_crossentropy")
@@ -36,8 +24,8 @@ class DetectorContainer:
 		# Load raw-ish data, parceled out into splits
 		data = h5py.File('data/train/binary/data.h5','r')
 		self.X_train = data['X_train']
-		self.y_masks_train = data['y_masks_train']
-		self.y_boxes = read_boxes()
+		y_masks_train = data['y_masks_train'][:]
+		self.y_masks_train = y_masks_train.reshape((3021,974,1732,1))
 
 		self.X_test_chunks = np.load('data/train/binary/X_test_det_chunk_seqs_'+str(n)+'.npy')
 		self.X_test_locations = np.load('data/train/binary/X_test_det_loc_seqs_'+str(n)+'.npy')
@@ -45,33 +33,44 @@ class DetectorContainer:
 		self.y_test_coverage = y_test_coverage.reshape((y_test_coverage.shape[0], y_test_coverage.shape[1], 1))
 
 		self.n = n
+
+		self.datagen_args = datagen_args
 		
 		eval_data = h5py.File("data/test_stg1/binary/eval_data.h5",'r')
 		self.X_eval = eval_data['X']
 		self.filenames_eval = np.load("data/test_stg1/binary/y_filenames.npy")
 
 	def sample_gen(self,batch_size):
-		random.seed(1) # For reproducibility
-		chunk_sequences = []
-		location_sequences = []
-		coverage_sequences = []
+		seed = 1 # For reproducibility
+
+		image_datagen = ImageDataGenerator(**self.datagen_args)
+		mask_datagen = ImageDataGenerator(**self.datagen_args)
+
+		image_gen = image_datagen.flow(self.X_train,None,batch_size=batch_size,seed=seed)
+		mask_gen = mask_datagen.flow(self.y_masks_train,None,batch_size=batch_size,seed=seed)
+
 		while True:
-			index = random.sample(range(len(self.X_train)),1)[0]
-			chunk_matrix = chunk_image(self.n,self.X_train[index])
-			coverage_matrix = chunk_mask(self.n, chunk_matrix, self.y_masks_train[index])
-			chunk_sequence, coverage_sequence, location_sequence = detector_sequencer(chunk_matrix, coverage_matrix)
-			chunk_sequences.append(chunk_sequence)
-			coverage_sequences.append(coverage_sequence)
-			location_sequences.append(location_sequence)
-			if len(chunk_sequences) == batch_size:
-				chunk_sequences = pad_sequences(chunk_sequences).astype(np.float32)
-				location_sequences = pad_sequences(location_sequences).astype(np.float32)
-				coverage_sequences = pad_sequences(coverage_sequences).astype(np.float32)
-				coverage_sequences = coverage_sequences.reshape((coverage_sequences.shape[0], coverage_sequences.shape[1], 1))
-				yield [chunk_sequences, location_sequences], coverage_sequences
-				chunk_sequences = []
-				location_sequences = []
-				coverage_sequences = []
+			images = image_gen.next()
+			masks = mask_gen.next()
+			masks = masks.reshape((batch_size,974,1732))
+
+			chunk_sequences = []
+			location_sequences = []
+			coverage_sequences = []
+			for i in range(len(images)):
+				chunk_matrix = chunk_image(self.n,images[i])
+				coverage_matrix = chunk_mask(self.n, chunk_matrix, masks[i])
+				chunk_sequence, coverage_sequence, location_sequence = detector_sequencer(chunk_matrix, coverage_matrix)
+				chunk_sequences.append(chunk_sequence)
+				coverage_sequences.append(coverage_sequence)
+				location_sequences.append(location_sequence)
+
+			chunk_sequences = pad_sequences(chunk_sequences).astype(np.float32)
+			location_sequences = pad_sequences(location_sequences).astype(np.float32)
+			coverage_sequences = pad_sequences(coverage_sequences).astype(np.float32)
+			coverage_sequences = coverage_sequences.reshape((coverage_sequences.shape[0], coverage_sequences.shape[1], 1))
+
+			yield [chunk_sequences, location_sequences], coverage_sequences
 
 	def train(self, weight_file=None, nb_epoch=40, batch_size=500, samples_per_epoch=10000):
 		model_folder = 'experiments/' + self.name + '/weights/'
