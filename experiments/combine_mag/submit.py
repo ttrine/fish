32c3a3e6e17df6b4b55b55929d64b17cfd896f08
Tyler Trine <trine.tyler@gmail.com>
2017-02-21 23:10:56 -0500

    Add submit.py to experiment subfolders to make generating submissions as easy as possible

--- experiments/combine_mag/submit.py
+++ experiments/combine_mag/submit.py
@@ -1,10 +1,6 @@
 from fish.classify import ClassifierContainer
 from experiments.combine_mag.train import construct
 
-def generate_submission(name, weight_file, clip):
-	c = ClassifierContainer('combine_mag',construct(128),128,"adam")
-	c.evaluate('74-1.0833.hdf5', clip=True)
-
 if __name__ == '__main__':
 	import sys # basic arg parsing, infer name
 	name = sys.argv[0].split('/')[-2]
