24f98355596d74bfc4bc0c76c3682563b1a7d3a1
Tyler Trine <trine.tyler@gmail.com>
2017-02-22 13:02:30 -0500

    Extend log parser to parse both coverage and class loss. Add combine_weight_switch to test what happens when we resume training on combine_weight with readjusted loss weights

--- analysis/training/log_parser.py
+++ analysis/training/log_parser.py
@@ -1,16 +1,25 @@
 import csv
+import numpy as np
 
 def parse_losses(experiment):
 	path = "experiments/"+experiment+"/log.out"
 	log = file(path,'r')
 	lines = log.readlines()
-	val_losses = [[float(line.split('val_loss: ')[-1].split('\n')[0])] for line in lines if 'val_loss: ' in line]
+
+	cov_train_losses = [float(line.split(' - loss: ')[-1].split('coverage_loss: ')[1].split(' - ')[0]) for line in lines if 'val_loss: ' in line]
+	class_train_losses = [float(line.split(' - loss: ')[-1].split('class_loss: ')[1].split(' - ')[0]) for line in lines if 'val_loss: ' in line]
+	
+	cov_test_losses = [float(line.split(' - loss: ')[-1].split('val_coverage_loss: ')[1].split(' - ')[0]) for line in lines if 'val_loss: ' in line]
+	class_test_losses = [float(line.split(' - loss: ')[-1].split('val_class_loss: ')[1].split(' - ')[0]) for line in lines if 'val_loss: ' in line]
+
+	data = np.array(zip(cov_train_losses, class_train_losses, cov_test_losses, class_test_losses))
 
 	# Dump to 1 column "csv"
 	outpath = "analysis/training/"+experiment+".csv"
 	f = file(outpath,'wb')
 	w = csv.writer(f)
-	w.writerows(val_losses)
+	w.writerow(['cov_train','class_train','cov_test','class_test'])
+	w.writerows(data)
 	f.close()
 
 if __name__ == '__main__':
