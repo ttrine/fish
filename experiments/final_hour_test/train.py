9219589256299c5a685cea8ce5a9cd774cc8b747
Tyler Trine <trine.tyler@gmail.com>
2017-02-26 15:06:12 -0500

    Reinstate detector for testing purposes. Add final_hour_test to assess validity of and obtain benchmarks for detection accuracy.

---
+++ experiments/final_hour_test/train.py
@@ -0,0 +1,55 @@
+from keras.models import Model
+
+from keras.layers import Input, Dense, Flatten, merge, Reshape, ZeroPadding2D, Convolution2D, MaxPooling2D, BatchNormalization
+from keras.layers.core import Masking, RepeatVector
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.recurrent import LSTM
+
+from fish.detect import DetectorContainer
+
+def construct():
+	imgs = Input(shape=(974, 1732, 3))
+	batch = BatchNormalization()(imgs)
+
+	# Shared CNN for learning representations common to detection and classification.
+	conv1 = ZeroPadding2D((3, 3))(batch)
+	conv1 = Convolution2D(16, 5, 5, activation='relu')(conv1)
+	conv1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+	conv2 = ZeroPadding2D((3, 3))(conv1)
+	conv2 = Convolution2D(32, 5, 5, activation='relu')(conv2)
+	conv2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+	conv3 = ZeroPadding2D((3, 3))(conv2)
+	conv3 = Convolution2D(64, 5, 5, activation='relu')(conv3)
+	conv3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+	conv4 = ZeroPadding2D((3, 3))(conv3)
+	conv4 = Convolution2D(128, 5, 5, activation='relu')(conv4)
+	conv4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+	conv5 = ZeroPadding2D((1, 1))(conv4)
+	conv5 = Convolution2D(256, 3, 3, activation='relu')(conv5)
+	conv5 = MaxPooling2D(pool_size=(2, 2))(conv5)
+
+	conv6 = ZeroPadding2D((1, 1))(conv5)
+	conv6 = Convolution2D(256, 3, 3, activation='relu')(conv6)
+	conv6 = ZeroPadding2D((2, 2))(conv6)
+	conv6 = Convolution2D(1, 3, 3, activation='relu')(conv6)
+	conv6 = MaxPooling2D(pool_size=(2, 2))(conv6)
+
+	# Shave off channel dimension
+	pred_mat = Reshape((16,28))(conv6)
+
+	return Model(input=imgs,output=pred_mat)
+
+if __name__ == '__main__':
+	import sys # basic arg parsing, infer name
+	name = sys.argv[0].split('/')[-2]
+	
+	if len(sys.argv) < 4:
+		print "Usage: train nb_epoch batch_size samples_per_epoch"
+		sys.exit()
+
+	model = DetectorContainer(name,construct(),64,"adam")
+	model.train(nb_epoch=int(sys.argv[1]), batch_size=int(sys.argv[2]), samples_per_epoch=int(sys.argv[3]))
---
+++ fish/detect.py
@@ -0,0 +1,115 @@
+import os, sys, csv
+import random
+import numpy as np
+import pandas
+import h5py
+
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+from keras.preprocessing.sequence import pad_sequences
+
+from keras.preprocessing.image import ImageDataGenerator
+
+from fish.chunk import chunk_mask, chunk_image
+from fish.sequence import train_sequencer, eval_sequencer
+
+class DetectorContainer:
+	def __init__(self,name,model,n,optimizer,datagen_args=dict(),callbacks=[]):
+		# Set instance variables
+		self.name = name
+		self.n = n
+		self.datagen_args = datagen_args
+
+		# Compile model
+		model.compile(optimizer=optimizer, loss="binary_crossentropy")
+
+		self.model = model
+		
+		data = h5py.File('data/train/binary/data.h5','r')
+
+		# Load train data
+		self.X_train = data['X_train']
+
+		y_masks_train = data['y_masks_train'][:]
+		self.y_masks_train = y_masks_train.reshape((3021,974,1732,1))
+	
+		y_classes_train = np.load('data/train/binary/y_classes_train.npy')
+		self.y_classes_train = np_utils.to_categorical(pandas.factorize(y_classes_train, sort=True)[0])
+
+		# Load test data
+		self.X_test = data['X_test']
+		self.y_test_coverage = np.load('data/train/binary/y_test_coverage_mats_'+str(n)+'.npy')
+		self.y_classes_test = np.load('data/train/binary/y_classes_test_onehot.npy')
+
+		# Load eval data
+		eval_data = h5py.File("data/test_stg1/binary/eval_data.h5",'r')
+		self.X_eval = eval_data['X_eval']
+		self.filenames_eval = np.load("data/test_stg1/binary/y_filenames.npy")
+
+		self.callbacks = callbacks
+
+	# Returns a generator that produces sequences to train against
+	def sample_gen(self,batch_size):
+		seed = 1 # For reproducibility
+
+		image_datagen = ImageDataGenerator(**self.datagen_args)
+		mask_datagen = ImageDataGenerator(**self.datagen_args)
+
+		image_gen = image_datagen.flow(self.X_train,self.y_classes_train,batch_size=batch_size,seed=seed)
+		mask_gen = mask_datagen.flow(self.y_masks_train,None,batch_size=batch_size,seed=seed)
+
+		while True:
+			images, classes = image_gen.next()
+			masks = mask_gen.next()
+			if masks.shape != ((batch_size,974,1732,1)): # Fix unknown wrong-shape error during reshape
+				continue
+			masks = masks.reshape((batch_size,974,1732))
+
+			coverage_matrices = []
+			class_labels = []
+			for i in range(len(images)):
+				chunk_matrix = chunk_image(self.n,images[i])
+				coverage_matrix = chunk_mask(self.n, chunk_matrix, masks[i])
+				
+				coverage_matrices.append(coverage_matrix)
+				class_labels.append(classes[i])
+
+			coverage_matrices = np.array(coverage_matrices)
+			class_labels = np.array(class_labels)
+
+			yield images, coverage_matrices
+
+	def train(self, weight_file=None, nb_epoch=40, batch_size=500, samples_per_epoch=10000):
+		model_folder = 'experiments/' + self.name + '/weights/'
+		if not os.path.exists(model_folder):
+			os.makedirs(model_folder)
+
+		if weight_file is not None:
+			self.model.load_weights(model_folder+self.name+weight_file)
+		
+		model_checkpoint = ModelCheckpoint(model_folder+'{epoch:002d}-{val_loss:.4f}.hdf5', monitor='loss')
+		self.callbacks.append(model_checkpoint)
+		train_gen = self.sample_gen(batch_size)
+
+		self.model.fit_generator(train_gen, samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch, 
+			validation_data=(self.X_test,self.y_test_coverage), verbose=1, callbacks=self.callbacks)
+
+	''' TODO. Predict class for each image. '''
+	def evaluate(self,weight_file,clip=False):
+		self.model.load_weights('experiments/'+self.name+'/weights/'+weight_file)
+
+		print "Running inference..."
+
+		predictions = self.model.predict(self.X_eval, verbose=True)[1]
+
+		if clip:
+			predictions = np.clip(predictions,0.02, 0.98, out=None)
+
+		f = file('experiments/'+self.name+'/submission.csv','wb')
+		w = csv.writer(f)
+		w.writerow(['image','ALB','BET','DOL','LAG','NoF','OTHER','SHARK','YFT'])
+		rows = [[filename] for filename in self.filenames_eval]
+		[rows[i].extend(list(predictions[i])) for i in range(1000)]
+		w.writerows(rows)
+		f.close()
+		print "Done. Wrote experiments/"+self.name+"/submission.csv."
