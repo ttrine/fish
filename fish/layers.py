bccb369a93d3cb23bf72cf28f980200e0550e2fd
Tyler Trine <trine.tyler@gmail.com>
2017-02-28 12:13:03 -0500

    Special batch normalization that works

--- fish/layers.py
+++ fish/layers.py
@@ -114,9 +114,9 @@ class SpecialBatchNormalization(Layer):
         m = K.mean(x_nonempty, axis=-1, keepdims=True)
         std = K.sqrt(K.var(x_nonempty, axis=-1, keepdims=True) + self.epsilon)
         x_normed = (x_nonempty - m) / (std + self.epsilon)
-        # x_normed = self.gamma * x_normed + self.beta
-
         x_normed = T.set_subtensor(x[x.nonzero()], x_normed)
+        x_normed = self.gamma * x_normed + self.beta
+
 
         return x_normed
 
