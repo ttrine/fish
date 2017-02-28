ed60d7d5bcf3f005b65976aa77ef64a233f81fd7
Tyler Trine <trine.tyler@gmail.com>
2017-02-28 09:59:00 -0500

    Implement Special Batch Normalization to improve quality of batch statistics

--- fish/layers.py
+++ fish/layers.py
@@ -60,6 +60,98 @@ class GradientReversalLayer(Layer):
         base_config = super(GradientReversalLayer, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
+class SpecialBatchNormalization(Layer):
+    # Normalizes nonblack elements of the input only.
+    ## Designed for use as input to the first layer of a model
+    ## whose images are of varying size and placed in a padded tensor.
+    def __init__(self, epsilon=1e-5, mode=0, axis=-1, momentum=0.99,
+                 weights=None, beta_init='zero', gamma_init='one',
+                 gamma_regularizer=None, beta_regularizer=None, **kwargs):
+        self.supports_masking = True
+        self.beta_init = initializations.get(beta_init)
+        self.gamma_init = initializations.get(gamma_init)
+        self.epsilon = epsilon
+        self.mode = mode
+        self.axis = axis
+        self.momentum = momentum
+        self.gamma_regularizer = regularizers.get(gamma_regularizer)
+        self.beta_regularizer = regularizers.get(beta_regularizer)
+        self.initial_weights = weights
+        if self.mode == 0:
+            self.uses_learning_phase = True
+        super(SpecialBatchNormalization, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        shape = (input_shape[self.axis],)
+
+        self.gamma = self.gamma_init(shape, name='{}_gamma'.format(self.name))
+        self.beta = self.beta_init(shape, name='{}_beta'.format(self.name))
+        self.trainable_weights = [self.gamma, self.beta]
+
+        self.regularizers = []
+        if self.gamma_regularizer:
+            self.gamma_regularizer.set_param(self.gamma)
+            self.regularizers.append(self.gamma_regularizer)
+
+        if self.beta_regularizer:
+            self.beta_regularizer.set_param(self.beta)
+            self.regularizers.append(self.beta_regularizer)
+
+        self.running_mean = K.zeros(shape,
+                                    name='{}_running_mean'.format(self.name))
+        self.running_std = K.ones(shape,
+                                  name='{}_running_std'.format(self.name))
+        self.non_trainable_weights = [self.running_mean, self.running_std]
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+        self.built = True
+        self.called_with = None
+
+    def call(self, x, mask=None):
+        assert self.built, 'Layer must be built before being called'
+        input_shape = self.input_spec[0].shape
+
+        reduction_axes = [0]
+        broadcast_shape = [1] * (len(input_shape) - 1)
+        broadcast_shape[self.axis] = input_shape[self.axis]
+
+        self.called_with = x
+
+        # Remove padded elements before computing statistics
+        x_nonempty = x.nonzero_values()
+
+        x_normed, mean, std = K.normalize_batch_in_training(
+            x_nonempty, self.gamma, self.beta, reduction_axes,
+            epsilon=self.epsilon)
+
+        self.updates = [K.moving_average_update(self.running_mean, mean, self.momentum),
+                        K.moving_average_update(self.running_std, std, self.momentum)]
+
+        x_normed_running = K.batch_normalization(
+            x_nonempty, self.running_mean, self.running_std,
+            self.beta, self.gamma,
+            epsilon=self.epsilon)
+
+        # pick the normalized form of x corresponding to the training phase
+        x_normed = K.in_train_phase(x_normed, x_normed_running)
+        x_normed_masked = T.set_subtensor(x[x.nonzero()], x_normed)
+
+        return x_normed_masked
+
+    def get_config(self):
+        config = {"epsilon": self.epsilon,
+                  "mode": self.mode,
+                  "axis": self.axis,
+                  "gamma_regularizer": self.gamma_regularizer.get_config() if self.gamma_regularizer else None,
+                  "beta_regularizer": self.beta_regularizer.get_config() if self.beta_regularizer else None,
+                  "momentum": self.momentum}
+        base_config = super(SequenceBatchNormalization, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
 class SequenceBatchNormalization(Layer):
     # Implements sequence-wise normalization of feature maps. 
     ## Based heavily upon BatchNormalization layer code.
